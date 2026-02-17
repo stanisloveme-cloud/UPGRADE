8158a38adef01e3420d0ceb72705df118912249e
stanisloveme-cloud <stanislove.me@gmail.com>
2026-02-17 22:55:06 +0300

    fix: remove legacy pool code from prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,23 +1,14 @@
 
-import 'dotenv/config';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-    constructor() {
-
-        this.pool = pool;
-    }
-
     async onModuleInit() {
         await this.$connect();
     }
 
     async onModuleDestroy() {
-        // Disconnect Prisma client first
         await this.$disconnect();
-        // Then close the pool
-        await this.pool.end();
     }
 }
